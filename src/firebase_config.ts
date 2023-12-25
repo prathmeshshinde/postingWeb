2332528c7f175b526a49cf95db2555d09b47d379
prathmeshshinde <prathmeshshinde105@gmail.com>
2023-12-26 02:48:01 +0530

    updated tests

--- src/firebase_config.ts
+++ src/firebase_config.ts
@@ -1,10 +1,10 @@
 export const config = {
   firebaseConfig: {
-    apiKey: "AIzaSyBYsjB_tH_caYza0u94yN-CRHbyh4v03yA",
-    authDomain: "posting-v3-a20ce.firebaseapp.com",
-    projectId: "posting-v3-a20ce",
-    storageBucket: "posting-v3-a20ce.appspot.com",
-    messagingSenderId: "289482344836",
-    appId: "1:289482344836:web:5c1b9f57448983619375d1",
+    apiKey: "AIzaSyCFy0AvDYoIlx8FPN0vnlA_Yur7U0FC7XA",
+    authDomain: "posting-v3-3f0b1.firebaseapp.com",
+    projectId: "posting-v3-3f0b1",
+    storageBucket: "posting-v3-3f0b1.appspot.com",
+    messagingSenderId: "12740497053",
+    appId: "1:12740497053:web:75ae456bb83d210bb2ce74",
   },
 };
